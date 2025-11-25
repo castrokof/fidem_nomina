9410b0fec874a25cf6e575aef43e7db53dc9e5d9
Jhonnathan Castro Galeano <castrokof@gmail.com>
2025-11-25 15:08:25 +0000

    Creacion de middleware farmacia se incluye rol admin

---
+++ app/Http/Middleware/PermisoFarmacia.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class PermisoFarmacia
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+         if ((session()->get('rol_nombre') == ('administrador')) || (session()->get('rol_nombre') == ('farmacia')))
+
+        return $next($request);
+
+        abort(403, "No tienes autorización para ingresar.");
+    }
+}
